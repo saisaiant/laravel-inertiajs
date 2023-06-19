ed67a3ac657ebc5db66d991708d4d7849e4f747e
ko sai <saisaiant@gmail.com>
2023-06-19 21:55:02 +0630

    install package

---
+++ resources/js/Pages/Test.vue
@@ -0,0 +1,20 @@
+<template>
+    Hello
+    <button @click="count++">+</button>
+    {{ count }}
+    <button @click="count--">-</button>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
